1f345ae6e2e9eb7a2a56d94f33d8af76ca3dd03f
Eduard Makarov <eduard.makarov.jetup@gmail.com>
2023-03-14 16:40:21 +0200

    draft-typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...typeOrmConfig,
+      autoLoadEntities: true,
+    }),
+    PostsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config/index.ts
@@ -0,0 +1,20 @@
+import { Post } from 'src/posts/entities/post.entity';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: String(process.env.POSTGRES_PASSWORD),
+  database: process.env.DATABASE_NAME,
+  entities: [Post],
+  migrationsRun: false,
+  logging: true,
+  migrationsTableName: 'migration',
+  migrations: [
+    __dirname + '/migration/**/*.ts',
+    __dirname + '/migration/**/*.js',
+  ],
+  synchronize: false,
+};
